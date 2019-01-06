90041752e2ecb600e997ebcd8d49414e3761de8e
RitikGupta9125 <Ritikgupta912536@gmail.com>
2019-01-07 00:05:08 +0530

    Intial phase

---
+++ app/src/main/java/com/example/android/moodymausam/utilities/NetworkUtils.java
@@ -0,0 +1,115 @@
+package com.example.android.moodymausam.utilities;
+
+/*
+ * These utilities will be used to communicate with the weather servers.
+ * */
+
+import android.net.Uri;
+import android.util.Log;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public final class NetworkUtils {
+
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String DYNAMIC_WEATHER_URL =
+            "https://andfun-weather.udacity.com/weather";
+
+    private static final String STATIC_WEATHER_URL =
+            "https://andfun-weather.udacity.com/staticweather";
+
+    private static final String FORECAST_BASE_URL = STATIC_WEATHER_URL;
+
+
+    /*The format we want in our API to return*/
+    private static final String format = "json";
+    /*The units we want in our API to return*/
+    private static final String units = "metric";
+    /*The number of days we want in our API to return*/
+    private static final int numDays = 14;
+
+    final static String QUERY_PARAM = "q";
+    final static String LAT_PARAM = "lat";
+    final static String LON_PARAM = "lon";
+    final static String FORMAT_PARAM = "mode";
+    final static String UNITS_PARAM = "units";
+    final static String DAYS_PARAM = "cnt";
+
+    /*
+     * Builds the URL used to talk to the weather server using a location. This Location is based
+     * on the query capabilities of the weather provider that we are using.
+     *
+     * @param  locationQuery The location that will query for.
+     * @return The URL to use to query the weather server.
+     * */
+    public static URL buildUrl(String locationQuery) {
+
+        Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM, locationQuery)
+                .appendQueryParameter(FORMAT_PARAM, format)
+                .appendQueryParameter(UNITS_PARAM, units)
+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        Log.v(TAG, "Built URI " + url);
+        return url;
+    }
+
+    /*
+    * Builds the URL used to talk to the weather server using latitude and longitude of a location.
+    *
+    * @param lat  The latitude of the location.
+    * @param lon  The longitude of the location.
+    * @return  The URL to use to query the weather server.
+    * */
+
+    public static URL builtUrl(double lat, double lon){
+        //Later code
+        return null;
+    }
+
+
+
+
+    /*
+     * This method returns the entire result from HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of HTTP response
+     * @throws IOException Related to network and stream reading
+     * */
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        }finally {
+            urlConnection.disconnect();
+        }
+    }
+
+}
+
+
