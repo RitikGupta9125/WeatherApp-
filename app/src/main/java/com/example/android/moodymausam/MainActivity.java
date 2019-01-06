1e42b7850b06eefd700760d69e0ffc2fe8d8666d
RitikGupta9125 <Ritikgupta912536@gmail.com>
2019-01-07 03:21:28 +0530

    Add Networking

--- app/src/main/java/com/example/android/moodymausam/MainActivity.java
+++ app/src/main/java/com/example/android/moodymausam/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.android.moodymausam;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.example.android.moodymausam.data.SunshinePreferences;
+import com.example.android.moodymausam.utilities.NetworkUtils;
+import com.example.android.moodymausam.utilities.OpenWeatherJsonUtils;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,31 +24,76 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        mWeatherTextView = (TextView)findViewById(R.id.tv_weather_data);
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
-        // An Array of string that contain the dummy weather data
+        /*Once all our views are setup.Call loadWeatherData method to perform the network request to get data  */
 
-        String [] dummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-                "Friday - Thunderstorms - 21°C / 9°C",
-                "Saturday - Thunderstorms - 16°C / 7°C",
-                "Sunday - Rainy - 16°C / 8°C",
-                "Monday - Partly Cloudy - 15°C / 10°C",
-                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                "Sat, May 28 - Meteors - 16°C / 7°C",
-                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
+        loadWeatherData();
+    }
 
-        //Append the dummy data strings with TextView
-        for(String dummyWeatherDay : dummyWeatherData){
-            mWeatherTextView.append(dummyWeatherDay + "\n\n\n");
-        }
+
+    /*
+    * This method will get the user's preferred location for weather,and then tell
+    * doInBackground method to get the weather data in background.
+    * */
+    private void loadWeatherData(){
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        //call the AsyncTask
+        new FetchWeatherTask().execute(location);
 
     }
+
+
+    /*
+     * @Class FetchWeatherTask class that extends AsycnTask to perform network requests.
+     */
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+        //Override doInBackground method to perform network requests.
+        @Override
+        protected String[] doInBackground(String... strings) {
+            /*If there is no zip code then there is nothing to lookup.*/
+            if (strings.length == 0) {
+                return null;
+            }
+
+            String location = strings[0];
+
+            //Pass the location var to buildUrl method in NetworkUtilities class for Request URL
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+
+
+            try {
+                String jsonWeatherResponse = NetworkUtils
+                        .getResponseFromHttpUrl(weatherRequestUrl);
+
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils
+                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+
+
+                return simpleJsonWeatherData;
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+
+        /*Override the onPostExecute method to display  the resultsof network request.*/
+
+        @Override
+        protected void onPostExecute(String[] weatherData) {
+            if (weatherData != null) {
+                //Append to string in the TextView
+                for (String weatherString : weatherData) {
+                    mWeatherTextView.append((weatherString) + "\n\n\n");
+                }
+            }
+        }
+    }
+
+
 }
+
